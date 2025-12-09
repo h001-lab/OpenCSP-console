68de30fa67aa0189662f2ae8b3586d20c412aacf
5y1ee <yunisy51@gmail.com>
2025-12-10 00:52:28 +0900

    chore(be): H2 테스트 의존성 추가

--- be/build.gradle.kts
+++ be/build.gradle.kts
@@ -61,6 +61,7 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.projectreactor:reactor-test")
     testImplementation("org.springframework.security:spring-security-test")
+    testImplementation("com.h2database:h2")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
 	// Swagger-UI
